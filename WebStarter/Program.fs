660e495fba27c1ae38542e68ba1a6577fd13f405
mausch <mauricioscheffer@gmail.com>
2010-04-25 23:26:47 -0300

    removed checking other framework version, it only works on 4.0 anyway

--- WebStarter/Program.fs
+++ WebStarter/Program.fs
@@ -12,16 +12,9 @@ let main args =
             then prg + " (x86)"
             else prg
             
-    let webserver90 = Path.Combine(progfile, @"Common Files\microsoft shared\DevServer\9.0\WebDev.WebServer.EXE")
-    let webserver100v2 = Path.Combine(progfile, @"Common Files\microsoft shared\DevServer\10.0\WebDev.WebServer20.EXE")
-    let webserver100v4 = Path.Combine(progfile, @"Common Files\microsoft shared\DevServer\10.0\WebDev.WebServer40.EXE")
-    let webserver = 
-        if File.Exists(webserver100v2) then 
-            if Environment.Version.Major = 2 
-                then webserver100v2
-                else webserver100v4
-        elif File.Exists(webserver90) then webserver90 
-        else failwith "No ASP.NET dev web server found."
+    let webserver = Path.Combine(progfile, @"Common Files\microsoft shared\DevServer\10.0\WebDev.WebServer40.EXE")
+    if not (File.Exists webserver)
+        then failwith "No ASP.NET dev web server found."
     
     let webSitePath = Directory.GetParent(Directory.GetCurrentDirectory()).FullName
     let port = Random().Next(10000, 65535)
