b67437621ed6a7ce52a668798a0cca463aa7e3cb
mausch <mauricioscheffer@gmail.com>
2010-04-30 20:07:58 -0300

    inline, simplest controller

--- Sample/Global.asax.fs
+++ Sample/Global.asax.fs
@@ -7,6 +7,7 @@ open System.Web.Routing
 open System.Collections.Specialized
 open FSharpMvc.RouteCollectionExtensions
 open FSharpMvc.Combinators
+open FSharpMvc.Result
 open Actions
 
 type MvcApplication() =
@@ -20,6 +21,7 @@ type MvcApplication() =
         //get "action6" action6
         //post "action6" (formAction postAction6Easy)
         post "action6" (bindForm postAction6Easier <@ postAction6Easier @>)
+        get "inline" (fun _ -> content "<h1>Hello world!</h1>")
 
     member this.Application_Start() =
         this.RegisterRoutes RouteTable.Routes
