0972e25c84e4be8d2f3ea848578e8cb8040a9a5e
mausch <mauricioscheffer@gmail.com>
2010-05-20 01:04:59 -0300

    some extension methods to deal with reflected functions

---
+++ FSharpMVC.Tests/ExtensionsTests.fs
@@ -0,0 +1,14 @@
+﻿module ExtensionsTests
+
+open Xunit
+open Microsoft.FSharp.Reflection
+open Extensions
+
+[<Fact>]
+let InvokeFunctionTests() =
+    let f a b c = a + b + c
+    let r = FSharpValue.InvokeFunction f [1;2;3]
+    Assert.NotNull r
+    Assert.IsType<int> r |> ignore
+    Assert.Equal(6, r :?> int)
+    ()
--- FSharpMVC/Extensions.fs
+++ FSharpMVC/Extensions.fs
@@ -1,5 +1,8 @@
 ﻿module Extensions
 
+open System
+open System.Reflection
+open Microsoft.FSharp.Reflection
 open System.Text.RegularExpressions
 
 type CaptureCollection with
@@ -11,3 +14,25 @@ type GroupCollection with
     member this.Groups
         with get() =
             this |> Seq.cast<Group> |> Seq.toArray
+
+type FSharpType with
+    static member GetFlattenedFunctionElements (functionType: Type) =
+        let domain, range = FSharpType.GetFunctionElements functionType
+        if not (FSharpType.IsFunction range)
+            then domain::[range]
+            else domain::FSharpType.GetFlattenedFunctionElements(range)
+
+
+
+type FSharpValue with
+    static member InvokeFunction (f: obj) (args: obj list): obj =
+        let ft = f.GetType()
+        if not (FSharpType.IsFunction ft)
+            then failwith "Not a function!"
+        let domain, range = FSharpType.GetFunctionElements ft
+        let fsft = typedefof<FSharpFunc<_,_>>.MakeGenericType [| domain; range |]
+        let bindingFlags = BindingFlags.InvokeMethod ||| BindingFlags.Instance ||| BindingFlags.Public
+        let r = fsft.InvokeMember("Invoke", bindingFlags, null, f, [| List.head args |])
+        if not (FSharpType.IsFunction(r.GetType()))
+            then box r
+            else FSharpValue.InvokeFunction r (List.tail args)
