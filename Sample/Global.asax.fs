72f7762a6a30297d41a592c8b0be7793b2ec0a55
mausch <mauricioscheffer@gmail.com>
2010-07-08 20:20:26 -0300

    cleanup

--- Sample/Global.asax.fs
+++ Sample/Global.asax.fs
@@ -9,26 +9,19 @@ open Figment.Routing
 open Figment.RoutingConstraints
 open Figment.Actions
 open Figment.Binding
+open Figment.Filters
 
 type MvcApplication() =
     inherit HttpApplication()
 
     member this.Application_Start() = 
-        let bindInt (parameter: string) (f: int -> 'b) (ctx: ControllerContext) =
-            let r = bindSingleParameter parameter ctx.Controller.ValueProvider (bindErrorDefault 0) ctx
-            f r
+        let routes = [
+                        ("hi", content "<h1>Hello World!</h1>")
+                     ]
+        let routes = routes |> Filters.apply (cache (OutputCacheParameters()))
         get "hi" (content "<h1>Hello World!</h1>")
         get "showform" (view "action2viewname" ())
-        let action6Get (firstname: string) (lastname: string) (age: int) = 
+        let nameAndAge (firstname: string) (lastname: string) (age: int) = 
             Result.content (sprintf "Hello %s %s, %d years old" firstname lastname age)
-        let b1 = bind "firstname" action6Get
-        let b1 b c a = b1 a b c
-        let b1 = bind "lastname" b1
-        let b1 b c a = b1 a b c
-        let b1 = bindInt "age" b1
-        let b1 a = b1 a a a
-        let cachedB1 = b1 |> Filters.cache (OutputCacheParameters(Duration = 60))
-        get "action6" b1
-        get "route/{firstname}/{lastname}/{age}" cachedB1
-        getS "route/{firstname:%s}/{lastname:%s}/{age:%d}" action6Get
+        getS "route/{firstname:%s}/{lastname:%s}/{age:%d}" nameAndAge
         ()
