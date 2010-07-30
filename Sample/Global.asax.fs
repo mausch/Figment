6b38298b9804f90853bf847834ddca1f3c2f99f9
mausch <mauricioscheffer@gmail.com>
2010-07-30 01:08:09 -0300

    wing beats integration

--- Sample/Global.asax.fs
+++ Sample/Global.asax.fs
@@ -10,11 +10,19 @@ open Figment.RoutingConstraints
 open Figment.Actions
 open Figment.Binding
 open Figment.Filters
+open WingBeats.Xhtml
+open WingBeats.Xml
 
 type MvcApplication() =
     inherit HttpApplication()
 
+    let wbview (n: Node) (ctx: ControllerContext) = 
+        Result.content <| Renderer.RenderToString n
+
     member this.Application_Start() = 
+        //ViewEngines.Engines.Clear();
+        //ViewEngines.Engines.Add(WingBeats.Mvc.WingBeatsTemplateEngine(System.Reflection.Assembly.GetExecutingAssembly()));
+
         let routes = [
                         ("hi", content "<h1>Hello World!</h1>")
                      ]
@@ -24,4 +32,13 @@ type MvcApplication() =
         let nameAndAge (firstname: string) (lastname: string) (age: int) = 
             Result.content (sprintf "Hello %s %s, %d years old" firstname lastname age)
         getS "route/{firstname:%s}/{lastname:%s}/{age:%d}" nameAndAge
+
+        let e = XhtmlElement()
+        let r = e.Html [
+                    e.Body [
+                        e.H1 [ &"Hello World from Wing Beats" ]
+                    ]
+                ]
+        get "wingbeats" (wbview r)
+
         ()
