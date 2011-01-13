89ccdfb4b5e05c09e831086b61c5b7d0a09ea413
mausch <mauricioscheffer@gmail.com>
2011-01-13 12:44:43 -0300

    session as dictionary (incomplete)

--- Figment/Extensions.fs
+++ Figment/Extensions.fs
@@ -1,6 +1,8 @@
 ﻿namespace Figment
 
 open System
+open System.Collections
+open System.Collections.Generic
 open System.Reflection
 open System.Web
 open System.Web.Mvc
@@ -27,8 +29,6 @@ module Extensions =
                 then domain::[range]
                 else domain::FSharpType.GetFlattenedFunctionElements(range)
 
-
-
     type FSharpValue with
         static member InvokeFunction (f: obj) (args: obj list): obj =
             let ft = f.GetType()
@@ -41,11 +41,6 @@ module Extensions =
             if not (FSharpType.IsFunction(r.GetType()))
                 then box r
                 else FSharpValue.InvokeFunction r (List.tail args)
-
-
-    type ControllerContext with
-        member x.UrlHelper with get() = UrlHelper(x.RequestContext)
-        member x.Cache with get() = x.HttpContext.Cache
 
     type Cache with
         member x.GetOrAdd(key: string, valueFactory: string -> 'a, ?dependencies, ?absoluteExpiration, ?slidingExpiration, ?priority, ?onRemoveCallback): 'a = 
@@ -67,3 +62,60 @@ module Extensions =
         member x.files =
             x.Files.AllKeys
             |> Seq.map (fun k -> k, x.Files.[k])
+
+    type HttpSessionStateBase with
+        member x.asDict =
+            let notimpl() = raise <| NotImplementedException()
+            { new IDictionary<string,obj> with
+                member d.Count = x.Count
+                member d.IsReadOnly = false 
+                member d.Item 
+                    with get k = 
+                        let v = x.[k]
+                        if v = null
+                            then raise <| KeyNotFoundException(sprintf "Key '%s' not found" k)
+                            else v
+                    and set k v = x.Add(k,v)
+                member d.Keys = upcast ResizeArray<string>(x.Keys |> Seq.cast)
+                member d.Values = 
+                    let values = ResizeArray<obj>()
+                    for i in 0..x.Count-1 do
+                        values.Add x.[i]
+                    upcast values
+                member d.Add v = d.Add(v.Key, v.Value)
+                member d.Add(key,value) = 
+                    if key = null
+                        then raise <| ArgumentNullException("key")
+                    if d.ContainsKey key
+                        then raise <| ArgumentException(sprintf "Duplicate key '%s'" key, "key")
+                    x.Add(key,value)
+                member d.Clear() = x.Clear()
+                member d.Contains item = x.[item.Key] = item.Value
+                member d.ContainsKey key = x.[key] <> null
+                member d.CopyTo(array,arrayIndex) = notimpl()
+                member d.GetEnumerator() : IEnumerator<KeyValuePair<string,obj>> = notimpl()
+                member d.GetEnumerator() : IEnumerator = notimpl()
+                member d.Remove (item: KeyValuePair<string,obj>) = 
+                    if d.Contains item then
+                        x.Remove item.Key
+                        true
+                    else
+                        false
+                member d.Remove (key: string) = 
+                    let exists = d.ContainsKey key
+                    x.Remove key
+                    exists
+                member d.TryGetValue(key: string, value: byref<obj>) = 
+                    if d.ContainsKey key then
+                        value <- x.[key]
+                        true
+                    else
+                        false
+                }
+
+    type ControllerContext with
+        member x.UrlHelper = UrlHelper(x.RequestContext)
+        member x.Cache = x.HttpContext.Cache
+        member x.UserHostAddress = x.HttpContext.Request.UserHostAddress
+        member x.Session = x.HttpContext.Session
+        member x.SessionDict = x.HttpContext.Session.asDict
