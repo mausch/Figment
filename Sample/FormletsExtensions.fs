78a635d3a5c34bf2ff8f1d9b2bd4fa9e09b95ca6
mausch <mauricioscheffer@gmail.com>
2011-02-14 20:04:31 -0300

    xml docs, piping

--- Sample/FormletsExtensions.fs
+++ Sample/FormletsExtensions.fs
@@ -5,8 +5,15 @@ module FormletsExtensions =
     open Figment.Routing
     open Formlets
 
+    /// <summary>
+    /// Maps a page with a formlet and its handler
+    /// </summary>
+    /// <param name="url">URL</param>
+    /// <param name="formlet">Formlet to show and process</param>
+    /// <param name="page">Function taking an URL and rendered formlet and returning a wingbeats tree</param>
+    /// <param name="successHandler">When formlet is successful, run this function</param>
     let formAction url formlet page successHandler =
-        get url (fun _ -> Result.wbview (page url (renderToXml formlet)))
+        get url (fun _ -> formlet |> renderToXml |> page url |> Result.wbview)
         post url
             (fun ctx -> 
                 let env = EnvDict.fromFormAndFiles ctx.HttpContext.Request
